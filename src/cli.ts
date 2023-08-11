7a221b65c61999332ef1aa2601b00fb0d6853eed
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-11 19:53:36 +0700

    more refactor

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 import { mkdirSync, writeFileSync } from "fs";
 import type { Page } from "puppeteer-core";
 import { getEvents, getEvent } from "./calendar";
+import { createHash } from "crypto";
+import { GoogleCalendarItem } from "./types";
 
 const { values } = parseArgs({
   options: {
@@ -12,9 +14,50 @@ const { values } = parseArgs({
   },
 });
 
+const hash = (id: string) => createHash("sha1").update(id).digest("hex");
+
+function shouldSkip(item: GoogleCalendarItem) {
+  const lowercase = item.summary.toLowerCase();
+
+  // skip if event is tentative or not finalized
+  if (lowercase.includes("tba")) return "tba";
+  if (lowercase.includes("tbc")) return "tbc";
+  if (lowercase.includes("tbd")) return "tbd";
+  if (lowercase.includes("tentative")) return "tentative";
+  if (lowercase.includes("[duplicate]")) return "duplicate";
+
+  // skip if too recent (updated within 16 hours)
+  if (Date.now() - Date.parse(item.updated) < 16 * 60 * 60 * 1000) {
+    return "too_recently_updated";
+  }
+
+  return false;
+}
+
 if (values["list-events"]) {
-  const events = await getEvents();
-  console.log(events);
+  const calendar = await getEvents();
+  const out: {
+    id: string;
+    summary: string;
+    startDate: string;
+    skip: boolean | string;
+  }[] = [];
+  for (const event of calendar.items) {
+    const { id, summary } = event;
+    const startDate = (
+      "dateTime" in event.start ? event.start.dateTime : event.start.date
+    ).slice(0, 10);
+    const skip = shouldSkip(event);
+    out.push({ id, summary, startDate, skip });
+  }
+  out.sort((a, b) => {
+    return a.startDate.localeCompare(b.startDate);
+  });
+  console.table(out.map(({ id, ...x }) => x));
+
+  for (const [index, row] of out.entries()) {
+    console.log(`[${index}]\n    pnpm cli --render-event ${row.id}`);
+  }
 } else if (values["render-event"]) {
   const eventId = values["render-event"];
   const event = await getEvent(eventId);
