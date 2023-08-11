b0d40a3325dd604391f6805ceeb9d5c9da24495b
Thai Pangsakulyanont <dtinth@spacet.me>
2023-08-11 12:03:13 +0000

    add capture url

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,20 @@
 import { parseArgs } from "util";
 import axios from "axios";
+import { GoogleCalendar, GoogleCalendarItem } from "./types";
+import { mkdirSync, writeFileSync } from "fs";
 
 const { values } = parseArgs({
   options: {
     "list-events": { type: "boolean" },
+    "render-event": { type: "string" },
+    "capture-url": { type: "string" },
   },
 });
 
 const apiKey = "AIzaSyAD6j9p5yfSNIyCZYDXDTorJils96CJvOQ";
 
 if (values["list-events"]) {
-  const events = await axios.get(
+  const { data: events } = await axios.get<GoogleCalendar>(
     "https://www.googleapis.com/calendar/v3/calendars/tech.cal.th%40gmail.com/events",
     {
       params: {
@@ -21,5 +25,41 @@ if (values["list-events"]) {
       },
     }
   );
-  console.log(events.data);
+  console.log(events);
+} else if (values["render-event"]) {
+  const eventId = values["render-event"];
+  const { data: event } = await axios.get<GoogleCalendarItem>(
+    `https://www.googleapis.com/calendar/v3/calendars/tech.cal.th%40gmail.com/events/${eventId}`,
+    { params: { key: apiKey } }
+  );
+  const yearAndMonth = (
+    "dateTime" in event.start ? event.start.dateTime : event.start.date
+  ).slice(0, 7);
+  const screenshotUrl = `https://th.techcal.dev/?${new URLSearchParams({
+    month: yearAndMonth,
+    capture: eventId,
+  })}`;
+  console.log(screenshotUrl);
+} else if (values["capture-url"]) {
+  const targetUrl = values["capture-url"];
+  const options = { targetUrl };
+  const code = `(${async (page, { targetUrl }) => {
+    await page.setViewport({ width: 1200, height: 1200, deviceScaleFactor: 2 });
+
+    await page.goto(targetUrl);
+    await page.waitForSelector(".eventItem");
+    await page.waitForSelector("#capture-title");
+    await page.waitForSelector("#capture-stage");
+
+    // Capture the portion of #capture-stage
+    const element = await page.$("#capture-stage");
+    return await element.screenshot({ type: "png" });
+  }})(page, ${JSON.stringify(options)})`;
+  const { data } = await axios.post(
+    "http://localhost:20279/run",
+    { code },
+    { timeout: 60000, responseType: "arraybuffer" }
+  );
+  mkdirSync(".data", { recursive: true });
+  writeFileSync(".data/capture.png", data);
 }
